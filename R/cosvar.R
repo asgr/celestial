07dcf721d3ab1f85f16fb642a15720736b6a5239
Aaron Robotham <aaron.robotham@uwa.edu.au>
2015-05-22 23:25:14 +0800

    Adjusted cosmic variance calculation

--- R/cosvar.R
+++ R/cosvar.R
@@ -1,8 +1,10 @@
 cosvarcar=function(aside=50, bside=50, cside=50, subsets=1){
+  aside=abs(aside); bside=abs(bside); cside=abs(cside)
   temp=sort(c(aside,bside),decreasing = TRUE)
   aside=temp[1];bside=temp[2]
   x=((aside/bside)-1.0)**0.5
-  return(cv=(1.0-0.03*x)*(219.7-52.4*log10(aside*bside*291.0) + 3.21*(log10(aside*bside*291.0))^2)/sqrt(subsets*cside/291.0))
+  cv=as.numeric((1.0-0.03*x)*(219.7-52.4*log10(aside*bside*291.0) + 3.21*(log10(aside*bside*291.0))^2)/sqrt(subsets*cside/291.0))
+  return(cv)
 }
 
 cosvarsph=function(long = c(129, 141), lat = c(-2, 3), zmax=1, zmin=0, subsets=1, inunit='deg'){
@@ -17,7 +19,12 @@ cosvarsph=function(long = c(129, 141), lat = c(-2, 3), zmax=1, zmin=0, subsets=1
   CoDistLow = cosdistCoDist(z=zmin,H0=70,OmegaM=0.3)     
   CoDistHigh = cosdistCoDist(z=zmax,H0=70,OmegaM=0.3)
   cside=CoDistHigh-CoDistLow
+  area=skyarea(long = long, lat = lat, inunit = inunit, outunit='deg2')[1]
+  volume=cosvol(area=area, zmax = zmax, zmin=zmin, H0 = 70, OmegaM = 0.3, inunit='deg2')[1]
   aside=cos(mean(lat)*pi/180)*(abs(diff(long))/360)*(CoDistLow+cside/2)
   bside=(abs(diff(long))/180)*(CoDistLow+cside/2)
+  scale=sqrt(volume*1e9/(aside*bside*cside))
+  aside=aside*scale
+  bside=bside*scale
   return(cosvarcar(aside=aside, bside=bside, cside=cside, subsets=subsets))
 }
