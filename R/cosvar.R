42224bcb5d8348b2abc5b1eb61ab610bfeb4e87f
Aaron Robotham <aaron.robotham@uwa.edu.au>
2015-06-10 10:54:43 +0200

    Making ready for CRAN

--- R/cosvar.R
+++ R/cosvar.R
@@ -7,7 +7,7 @@ cosvarcar=function(aside=50, bside=50, cside=50, regions=1){
   return(cv)
 }
 
-cosvarsph=function(long = c(129, 141), lat = c(-2, 3), zmax=1, zmin=0, regions=1, inunit='deg'){
+cosvarsph=function(long = c(129, 141), lat = c(-2, 3), zmax=1, zmin=0, regions=1, inunit='deg', sep=":"){
   if(inunit %in% c('deg','amin','asec','rad','sex')==FALSE){stop('inunit must be one of deg, amin, asec, rad or sex')}
   if(length(long)==1){long=c(0,long)}
   if(length(lat)==1){lat=c(0,lat)}
@@ -19,7 +19,7 @@ cosvarsph=function(long = c(129, 141), lat = c(-2, 3), zmax=1, zmin=0, regions=1
   CoDistLow = cosdistCoDist(z=zmin,H0=70,OmegaM=0.3)     
   CoDistHigh = cosdistCoDist(z=zmax,H0=70,OmegaM=0.3)
   cside=CoDistHigh-CoDistLow
-  area=skyarea(long = long, lat = lat, inunit = inunit, outunit='deg2')[1]
+  area=skyarea(long = long, lat = lat, inunit = 'deg', outunit='deg2')[1]
   volume=cosvol(area=area, zmax = zmax, zmin=zmin, H0 = 70, OmegaM = 0.3, inunit='deg2')[1]
   aside=cos(mean(lat)*pi/180)*(abs(diff(long))/360)*(CoDistLow+cside/2)
   bside=(abs(diff(long))/180)*(CoDistLow+cside/2)
