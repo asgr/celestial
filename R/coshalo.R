171a647827511c489c71f380b1b1a605bccca79c
Aaron Robotham <aaron.robotham@uwa.edu.au>
2016-02-04 09:36:43 +0800

    changed approximate 1.76 to sqrt(3)

--- R/coshalo.R
+++ R/coshalo.R
@@ -12,13 +12,13 @@ coshaloMvirToSigma=function(Mvir=1e12, z=0, H0=100, OmegaM=0.3, OmegaL=1-OmegaM-
   if(Rho=='crit'){RhoVal=(cosgrowRhoCrit(z=z, H0=H0, OmegaM=OmegaM, OmegaL=OmegaL, OmegaR=OmegaR, Dist='Ang', ref=ref)/1e9)*((Lunit/1e3)^3)/Munit}
   if(Rho=='mean'){RhoVal=(cosgrowRhoMean(z=z, H0=H0, OmegaM=OmegaM, OmegaL=OmegaL, OmegaR=OmegaR, Dist='Ang', ref=ref)/1e9)*((Lunit/1e3)^3)/Munit}
   Sigma=(Mvir*sqrt(32*pi*g^3*DeltaVir*RhoVal/3))^(1/3)
-  if(Dim==2){Sigma=Sigma/1.76}
+  if(Dim==2){Sigma=Sigma/sqrt(3)}
   return(Sigma)
 }
 
 coshaloSigmaToMvir=function(Sigma=230, z=0, H0=100, OmegaM=0.3, OmegaL=1-OmegaM-OmegaR, OmegaR=0, Rho='crit', Dist='Co', DeltaVir=200, Munit=1, Lunit=1e6, Vunit=1e3, Dim=3, ref){
   if(!Dim %in% 2:3){stop("Dim must be 2 or 3!")}
-  if(Dim==2){Sigma=Sigma*1.76}
+  if(Dim==2){Sigma=Sigma*sqrt(3)}
   if(DeltaVir=='get'){
     DeltaVir=cosgrowDeltaVir(z=z, OmegaM=OmegaM, OmegaL=OmegaL, OmegaR=OmegaR, ref=ref)
     Rho='crit'
@@ -71,7 +71,7 @@ coshaloRvirToMvir=function(Rvir=162.635, z=0, H0=100, OmegaM=0.3, OmegaL=1-Omega
 
 coshaloSigmaToRvir=function(Sigma=230, z=0, H0=100, OmegaM=0.3, OmegaL=1-OmegaM-OmegaR, OmegaR=0, Rho='crit', Dist='Co', DeltaVir=200, Munit=1, Lunit=1e6, Vunit=1e3, Dim=3, ref){
   if(!Dim %in% 2:3){stop("Dim must be 2 or 3!")}
-  if(Dim==2){Sigma=Sigma*1.76}
+  if(Dim==2){Sigma=Sigma*sqrt(3)}
   if(DeltaVir=='get'){
     DeltaVir=cosgrowDeltaVir(z=z, OmegaM=OmegaM, OmegaL=OmegaL, OmegaR=OmegaR, ref=ref)
     Rho='crit'
@@ -107,13 +107,13 @@ coshaloRvirToSigma=function(Rvir=162.635, z=0, H0=100, OmegaM=0.3, OmegaL=1-Omeg
   if(Dist=='Ang'){scale=1}
   if(Dist=='Co'){scale=1+z}
   Sigma=(Rvir/scale)*((512*pi^3*g^3*DeltaVir^3*RhoVal^3)/27)^(1/6)
-  if(Dim==2){Sigma=Sigma/1.76}
+  if(Dim==2){Sigma=Sigma/sqrt(3)}
   return(Sigma)
 }
 
 coshaloSigmaToTvir=function(Sigma=230, Vunit=1e3, Tunit='K', Type='halo', Dim=3){
   if(!Dim %in% 2:3){stop("Dim must be 2 or 3!")}
-  if(Dim==2){Sigma=Sigma*1.76}
+  if(Dim==2){Sigma=Sigma*sqrt(3)}
   if(Type=='halo'){ft=1}
   if(Type=='gas'){ft=0.78}
   if(Tunit=='K'){convert=kNIST2010BoltzmannConstant}
